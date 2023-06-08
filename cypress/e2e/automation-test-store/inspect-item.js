0ed95571c02028389cc068ee3a9f9408b00027cd
alinaberlin <alina.ghetler@gmail.com>
2023-06-08 12:40:49 +0200

    Adding test for validation product item using item's index

--- cypress/e2e/automation-test-store/inspect-item.js
+++ cypress/e2e/automation-test-store/inspect-item.js
@@ -4,8 +4,11 @@
 describe("Inspect Automation Test Store items using chain of commands", () => {
     it("Click on the first item using item header", () => {
       cy.visit("https://www.automationteststore.com/");
-      cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click()
-  
+        cy.contains('Skinsheen Bronzer Stick').click()
     });
+    it("Click on the second product item using item's index", ()=>{
+        cy.visit("https://www.automationteststore.com/");
+        cy.get(".fixed_wrapper").find(".prdocutname").eq(1).click()
+    })
   });
   
