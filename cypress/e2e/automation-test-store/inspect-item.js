5f07f1071e544fdf77bb89950c12666b251d7457
alinaberlin <alina.ghetler@gmail.com>
2023-06-08 12:17:36 +0200

    Refactor 'inspect-item.js'test

---
+++ cypress/e2e/automation-test-store/inspect-item.js
@@ -0,0 +1,11 @@
+//// <reference types= "Cypress" />
+
+
+describe("Inspect Automation Test Store items using chain of commands", () => {
+    it("Click on the first item using item header", () => {
+      cy.visit("https://www.automationteststore.com/");
+      cy.get('.prdocutname').contains('Skinsheen Bronzer Stick').click()
+  
+    });
+  });
+  
