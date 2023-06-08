c51ea57c02a5fa4175b4f3b57a8da9a6c6d94c03
alinaberlin <alina.ghetler@gmail.com>
2023-06-08 22:20:39 +0200

    Refactor test for item

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -4,7 +4,9 @@
 describe("Test Contact Us form via Autmation Test Store", () => {
   it("Should be able to submit a successful submission via constact us form", () => {
     cy.visit("https://www.automationteststore.com/");
-    cy.get("a[href$='contact']").click();
+    cy.get("a[href$='contact']").click().then(function(containText){
+        cy.log("Click on the button " + containText.text())
+    } );
     //cy.get('.info_links_footer > :nth-child(5) > a').click()
     //a[contains(@href, 'contact')]
     //cy.xpath("//a[contains(@href, 'contact')]").click();
@@ -16,6 +18,7 @@ describe("Test Contact Us form via Autmation Test Store", () => {
     //cy.get("buttont[title='Submit']").click();
     cy.get("button[title='Submit']").click(); 
     cy.get('.mb40 > :nth-child(3)').should('have.text', 'Your enquiry has been successfully sent to the store owner!')
+    cy.log("Test has completed")
 
   });
 });
