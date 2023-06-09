1367f5fe6ebe8ab132e06869ea3a53e39b882617
alinaberlin <alina.ghetler@gmail.com>
2023-06-09 10:33:32 +0200

    Create new test items where to use variables

---
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.js
@@ -0,0 +1,12 @@
+
+/// <reference types="cypress" />
+
+describe("Verifying variables, cypress commands and jquery commands", () => {
+    it("Navigating to specific product pages", () => {
+        cy.visit("https://www.automationteststore.com/");
+        const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
+        makeupLink.click();
+        const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
+        skincareLink.click();
+    });
+})
