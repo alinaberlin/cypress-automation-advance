40664d8ad7821d361dc2db75653cb23dcf6030aa
alinaberlin <alina.ghetler@gmail.com>
2023-06-09 11:08:00 +0200

    Adding more clear solution

--- cypress/e2e/automation-test-store/variables-and-cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and-cy-commands.js
@@ -3,10 +3,20 @@
 
 describe("Verifying variables, cypress commands and jquery commands", () => {
     it("Navigating to specific product pages", () => {
-        cy.visit("https://www.automationteststore.com/");
-        const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
-        makeupLink.click();
-        const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
-        skincareLink.click();
+        // cy.visit("https://www.automationteststore.com/");
+        // const makeupLink = cy.get("a[href*='product/category&path=']").contains("Makeup")
+        // makeupLink.click();
+        // const skincareLink = cy.get("a[href*='product/category&path=']").contains("Skincare")
+        // skincareLink.click();
+
+        //Recomanded Approach
+        cy.get("a[href*='product/category&path=']").contains("Makeup").click();
+        cy.get("a[href*='product/category&path=']").contains("Skincare").click();
+        cy.get("h1 .maintext").then(($headerText) => {
+            const headerText = $headerText.text()
+            cy.log("Found header text: " + headerText)
+            expect(headerText).is.eq('Makeup')
+        })
+
     });
 })
