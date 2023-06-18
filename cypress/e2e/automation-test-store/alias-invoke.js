b54a99dec94e25ea132c471f769b24e14694a0c3
alinaberlin <alina.ghetler@gmail.com>
2023-06-18 16:52:59 +0200

    Add test for calculating the total of the products

--- cypress/e2e/automation-test-store/alias-invoke.js
+++ cypress/e2e/automation-test-store/alias-invoke.js
@@ -9,11 +9,19 @@ describe("Alias and invoce", () => {
       cy.get('@productThumbnail').its('length').should('be.gt', 5)
       cy.get('@productThumbnail').should('include', 'Seaweed Conditioner')
     });
-    it.only("Validate product thumbnail", () => {
+    it("Validate product thumbnail", () => {
         cy.visit("https://automationteststore.com/");
         cy.get(".thumbnail").as('productThumbnail')
         cy.get('@productThumbnail').should('have.length', 16)
         cy.get('@productThumbnail').find('.productcart').invoke('attr', 'title').should('include', 'Add to Cart')
         
+      });
+      it.only("Calculate the total of product", () => {
+        cy.visit("https://automationteststore.com/");
+        cy.get(".thumbnail").as('productThumbnail')
+        cy.get('@productThumbnail').find('.oneprice').each(($el, index, $list) => {
+          cy.log($el.text())
+            
+        })
       });
   });
